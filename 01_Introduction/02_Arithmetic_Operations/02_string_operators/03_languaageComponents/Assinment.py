1514d138a7894175272a4a915f94c50f279e81a3
osaimahesh21 <121642261+osaimahesh21@users.noreply.github.com>
2024-02-16 21:03:22 +0000

    class09

---
+++ 01_Introduction/02_Arithmetic_Operations/02_string_operators/03_languaageComponents/Assinment.py
@@ -0,0 +1,83 @@
+str1 = "abc"
+str2 = "abc"
+print(f"{str1 == str2 =}")
+print(f"{str1 >  str2 =}")
+print(f"{str1 >= str2 =}")
+print(f"{str1 <  str2 =}")
+print(f"{str1 <= str2 =}")
+print(f"{str1 != str2 =}")
+print()
+
+
+str1 = "abc"
+str2 = "bc"
+print(f"{str1 == str2 =}")
+print(f"{str1 >  str2 =}")
+print(f"{str1 >= str2 =}")
+print(f"{str1 <  str2 =}")
+print(f"{str1 <= str2 =}")
+print(f"{str1 != str2 =}")
+
+
+# Integer Comparison:
+
+num1 = 5
+num2 = 5
+print(f"{num1 == num2 =}")  # True
+print(f"{num1 >  num2 =}")  # False
+print(f"{num1 >= num2 =}")  # True
+print(f"{num1 <  num2 =}")  # False
+print(f"{num1 <= num2 =}")  # True
+print(f"{num1 != num2 =}")  # False
+print()
+
+num1 = 5
+num2 = 10
+print(f"{num1 == num2 =}")  # False
+print(f"{num1 >  num2 =}")  # False
+print(f"{num1 >= num2 =}")  # False
+print(f"{num1 <  num2 =}")  # True
+print(f"{num1 <= num2 =}")  # True
+print(f"{num1 != num2 =}")  # True
+
+# Float Comparison:
+
+float1 = 3.5
+float2 = 3.5
+print(f"{float1 == float2 =}")  # True
+print(f"{float1 >  float2 =}")  # False
+print(f"{float1 >= float2 =}")  # True
+print(f"{float1 <  float2 =}")  # False
+print(f"{float1 <= float2 =}")  # True
+print(f"{float1 != float2 =}")  # False
+print()
+
+float1 = 3.5
+float2 = 4.5
+print(f"{float1 == float2 =}")  # False
+print(f"{float1 >  float2 =}")  # False
+print(f"{float1 >= float2 =}")  # False
+print(f"{float1 <  float2 =}")  # True
+print(f"{float1 <= float2 =}")  # True
+print(f"{float1 != float2 =}")  # True
+
+# Boolean Comparison:
+
+bool1 = True
+bool2 = True
+print(f"{bool1 == bool2 =}")  # True
+# Booleans cannot be compared using '>' or '<'
+# Booleans cannot be compared using '>=' or '<='
+# Booleans cannot be compared using '>' or '<'
+# Booleans cannot be compared using '>=' or '<='
+print(f"{bool1 != bool2 =}")  # False
+print()
+
+bool1 = True
+bool2 = False
+print(f"{bool1 == bool2 =}")  # False
+# Booleans cannot be compared using '>' or '<'
+# Booleans cannot be compared using '>=' or '<='
+# Booleans cannot be compared using '>' or '<'
+# Booleans cannot be compared using '>=' or '<='
+print(f"{bool1 != bool2 =}")  # True
